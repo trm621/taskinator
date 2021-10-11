87d74fbe95925d023c5c292fefdde6d3eddcc61b
trm621 <t.maher621@gmail.com>
2021-10-11 16:44:51 -0400

    added reset function to taskFormHandler

--- js/script.js
+++ js/script.js
@@ -6,13 +6,20 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
+  if (!taskNameInput || !taskNameInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
     var taskDataObj = {
       name: taskNameInput,
       type: taskTypeInput
     };
 
     createTaskEl(taskDataObj);
-  }
+  };
 
   var createTaskEl = function(taskDataObj) {
       // create list item
